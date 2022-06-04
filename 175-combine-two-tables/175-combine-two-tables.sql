28af47aff87559cc900afdef370b1f79f68f9f51
Ansh <anshyadav4244@gmail.com>
2022-06-04 15:12:50 +0530

    Time: 1035 ms (5.01%), Space: 0B (100.00%) - LeetHub

--- 175-combine-two-tables/175-combine-two-tables.sql
+++ 175-combine-two-tables/175-combine-two-tables.sql
@@ -1,4 +1,3 @@
 SELECT firstName, lastName, city, state FROM PERSON 
 left JOIN ADDRESS
-on Person.PersonId = Address.PersonId
-order by person.firstName, person.lastName;
+on Person.PersonId = Address.PersonId;
